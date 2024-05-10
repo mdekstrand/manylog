fdf1b0d035ab94baa35def75384afb6c7dc38320
Michael Ekstrand <md@ekstrandom.net>
2024-05-10 13:20:00 -0400

    clean up records so they are serializable

--- manylog/worker/logging.py
+++ manylog/worker/logging.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import copy
 from logging import Handler, LogRecord
 
 from manylog.connection import Socket
@@ -13,8 +14,16 @@ class ZMQLogHandler(Handler):
         self.socket = socket
 
     def handle(self, record: LogRecord) -> LogRecord | bool:  # type: ignore
+        # copy so other handlers don't have a problem
+        record = copy.copy(record)
+
+        # update messages for copyability
         if not hasattr(record, "message"):
             record.message = record.msg % record.args
+        record.exc_info = None
+        record.exc_text = None
+        record.stack_info = None
+
         msg = LogMsg.create(record)
         data = msg.encode()
         self.socket.send(data)
